8d3201780479b5b9270a16b3ae94397357685a81
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-11-17 02:03:25 -0300

    components

---
+++ exe_perguntas01/lib/components/Questionario.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'Questao.dart';
+import 'resposta.dart';
+
+class Questionario extends StatelessWidget {
+  final int index;
+  final List<Map<String, Object>> listPerguntas;
+  final void Function() onChanged;
+  
+  const Questionario({super.key, required this.index, required this.listPerguntas, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, Object>> lista = listPerguntas[index]['resposta'] as List<Map<String, Object>>;
+    return Column(
+      children: [
+        Questao(texto: listPerguntas[index]['texto'] as String),
+        ...lista.map((map) {
+          return Resposta(texto: map['texto'] as String, onSelected: onChanged);
+        }).toList()
+      ],
+    );
+  }
+}
