46b3a0ba8ee6f405edaeab9990f0f6f15e6cdb16
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-11-17 02:36:49 -0300

    funcao reset

--- exe_perguntas01/lib/components/resultado.dart
+++ exe_perguntas01/lib/components/resultado.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
-  final String texto;
+  final int valor;
   final void Function() onReset;
-  const Resultado({super.key,required this.texto, required this.onReset});
+  const Resultado({super.key, required this.valor, required this.onReset});
+
+  String get textoResposta {
+    if (valor < 8) {
+      return 'Parabéns';
+    } else if (valor < 12) {
+      return 'Muito Bom!';
+    } else if (valor < 16) {
+      return 'Impressionante !';
+    } else {
+      return 'Nível Mestre !';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-        margin: const EdgeInsets.all(20),
-        child: Text(texto),
+    return Center(
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Container(
+          margin: const EdgeInsets.all(20),
+          child: Text(
+            textoResposta,
+            style: const TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
-        ElevatedButton.icon(onPressed: onReset, icon: const Icon(Icons.restore), label: Text(texto),),
-    ]);
+        ElevatedButton.icon(
+          onPressed: onReset,
+          icon: const Icon(Icons.restore),
+          label: const Text('Reste'),
+        ),
+      ]),
+    );
   }
 }
